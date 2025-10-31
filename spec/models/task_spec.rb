dff4bb1aaeedd12244598e0f5c004769f3484671
Fanon-hub <fanonwalker@gmail.com>
2025-10-31 11:34:39 +0300

    Fix system specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,28 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  describe 'Validations' do
-    context 'If the task Title is an empty string' do
-      it 'is not valid' do
-        task = Task.create(title: '', content: 'Create task content.')
-        expect(task).not_to be_valid
-        expect(task.errors[:title]).to include("can't be blank")
-      end
-    end
-
-    context 'If the task Content is an empty string' do
-      it 'is not valid' do
-        task = Task.create(title: 'Task Title', content: '')
-        expect(task).not_to be_valid
-        expect(task.errors[:content]).to include("can't be blank")
-      end
-    end
-    context 'If both the task Title and Content are provided' do
-      it 'is valid' do
-        task = Task.create(title: 'Task Title', content: 'Create task content.')
-        expect(task).to be_valid
+RSpec.describe 'Task management function', type: :system do
+  describe 'Registration function' do
+    context 'When registering a task' do
+      it 'The registered task is displayed' do
+        visit new_task_path
+        fill_in 'Title Title', with: 'Test Title'
+        fill_in 'Content', with: 'Test Content'
+        click_button 'Create Task'
+        expect(page).to have_content 'Test Title'
+        expect(page).to have_content 'Task was successfully created.'
       end
     end
   end
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'List display function' do
+    context 'When transitioning to the list screen' do
+      it 'A list of registered tasks is displayed' do
+        task = FactoryBot.create(:task)
+        second_task = FactoryBot.create(:second_task)
+        visit tasks_path
+        expect(page).to have_content task.title  # Positive: expects presence
+        expect(page).to have_content second_task.title
+      end
+    end
+  end
+
+  describe 'Detailed display function' do
+    context 'When transitioned to any task details screen' do
+      it 'The content of the task is displayed' do
+        task = FactoryBot.create(:task)
+        visit task_path(task)
+        expect(page).to have_content task.title  # Positive: expects presence
+        expect(page).to have_content task.content
+        expect(page).to have_content task.created_at.strftime('%Y-%m-%d %H:%M:%S UTC')
+      end
+    end
+  end
 end
