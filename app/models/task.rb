263a32dd09c8bf077d1641e97918bac2a8e1b7a1
Fanon-hub <fanonwalker@gmail.com>
2025-11-10 13:53:38 +0300

    Step 2: i18n, timezone, sort, specs, seeds, pagination

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, presence: { message: "can't be blank" }
-  validates :content, presence: { message: "can't be blank" }
+  validates :title, presence: { message: I18n.t('errors.messages.blank_title') }
+  validates :content, presence: { message: I18n.t('errors.messages.blank_content') }
 end
