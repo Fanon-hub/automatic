0127a2eb07a72df9dad7776df2c0c93e6b798add
Fanon-hub <fanonwalker@gmail.com>
2026-01-07 09:37:35 +0300

    Add application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-# app/controllers/sessions_controller.rb
+
 class SessionsController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,26 @@
-# app/controllers/application_controller.rb
-class ApplicationController < ActionController::Base
-  # This applies to all controllers by default
-  before_action :login_required
+class UsersController < ActionController
+  # before_action :login_required
   
   helper_method :current_user, :logged_in?
   
   private
   
-  # Method being skipped in UsersController for new/create actions
   def login_required
     unless logged_in?
       redirect_to login_path, alert: "ログインしてください"
     end
   end
   
-  # Method required for new/create actions in UsersController
   def logout_required
     if logged_in?
       redirect_to tasks_path, alert: "すでにログインしています"
     end
   end
   
-  # Method used in correct_user check
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
   
-  # Helper method to check login status
   def logged_in?
     current_user.present?
   end
