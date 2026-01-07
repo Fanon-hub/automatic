28d864d4a5a5e36385ccefe4623aa4db1845c2f7
Fanon-hub <fanonwalker@gmail.com>
2026-01-07 12:44:52 +0300

    Add precompiled assets for Propshaft

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   # Sessions
   get 'login', to: 'sessions#new', as: :login 
+  get '/login', to: 'sessions#new', as: :new_session
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: :logout
   
