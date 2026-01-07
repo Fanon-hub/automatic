daf8d1f29c486610a161e1673fcc30b9723cfecf
Fanon-hub <fanonwalker@gmail.com>
2026-01-07 12:31:13 +0300

    Add login route with named helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'tasks#index'
   
   # Sessions
-  get 'login', to: 'sessions#new', as: :new_session
-  post 'login', to: 'sessions#create', as: :create_session
-  delete 'logout', to: 'sessions#destroy', as: :destroy_session
+  get 'login', to: 'sessions#new', as: :login 
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: :logout
   
   # Regular users
   resources :users, only: [:new, :create, :show, :edit, :update]
